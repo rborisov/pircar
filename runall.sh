f2c58e2f803242dc21b9994ddce09f62f687c7ed
Roman Borisov <rborisoff@gmail.com>
2015-04-27 17:59:26 +0300

    cmake; make package builds the deb packet

--- runall.sh
+++ runall.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 RESULT=`pgrep indi-pirc`
 if [ "${RESULT:-null}" = null ]; then
-    ./indi-pirc/build/indi-pirc &
+    ./build/indi-pirc/indi-pirc &
 else
     echo "indi-pirc already running"
 fi
 RESULT=`pgrep radio-pirc`
 if [ "${RESULT:-null}" = null ]; then
-    ./radio-pirc/build/radio-pirc &
+    ./build/radio-pirc/radio-pirc &
 else
     echo "radio-pirc already running"
 fi
 RESULT=`pgrep browser-pirc`
 if [ "${RESULT:-null}" = null ]; then
-    ./browser-pirc/build/browser-pirc -w 320 -h 240 &
+    ./build/browser-pirc/browser-pirc -w 320 -h 240 &
 else
     echo "browser-pirc already running"
 fi
